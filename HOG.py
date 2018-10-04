959ad41289e074a5b72cdbc802ac802aade1585d
timjbaer <timjohnbaer@gmail.com>
2018-10-03 21:05:58 -0500

    Outlined HOG function

---
+++ HOG.py
@@ -0,0 +1,35 @@
+#Histograms of Oriented Gradients
+def create_cells(image):
+    return cells
+
+def gradient(vector):
+    return gradient
+
+def histogram(cell):
+    for pixel in cell:
+        # add gradient(pixel) to histogram
+    return histogram
+
+def create_blocks(image):
+    return block
+
+def concentate_histograms(block):
+    for cell in block:
+    # add histogram(cell) to a vector to_be_named
+    return to_be_named
+
+def normalize(vector):
+    return normalize_vector
+
+cells = create_cells(image)
+for cell in cells:
+    histogram(cell)
+
+blocks = create_blocks(image)
+for block in blocks:
+    normalize(concentate_histograms(blocks))
+
+
+
+
+
